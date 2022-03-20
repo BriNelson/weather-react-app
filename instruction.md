444460d02e2e7863336330fdc51df81cb2ff9e5a
Brinelson <ladofmusic@gmail.com>
2022-03-20 03:30:10 -0600

    some base stuff working

---
+++ instruction.md
@@ -0,0 +1,26 @@
+React - A JavaScript library for building user interfaces
+For this assignement we will be exploring the React Library by Facebook by building a simple weather app.
+
+Weather App Assignment - User Stories
+
+As a user, I want to be able to set my location.
+
+As a user, I want to be able to view the current tempature for my location.
+
+As a user, I want to choose between celius and ferinhight.
+
+As a user, I want to see a 7 day forcast for my location.
+
+As a user, I want to be able to see an hourly forcast for a selected day.
+
+
+
+Tips
+Work though the React Tutorial on the React Website.
+Another useful tutorial is Thinking in React
+Consider using Create React App to get started.
+Don't let JSX trip you up. Introducing JSX It is a syntax extension to Javascript and get transpiled to a JS object.
+Don't overlook Hooks. They are a new feature to React that allows developers to use state and other features without having to write a class.
+There are 2 types of React components. Class and function components. Pay attention to the syntax difference. Components and Props
+
+https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { Button } from '@mui/material';
 import './App.css';
 
+function Weather({temperature, weather, city}) {
+  
+  return (
+    <>
+      <h3>{city}</h3>
+      <p> {temperature}</p>
+      <p> {weather}</p>
+      
+  
+    </>
+  )
+}
+
+
 function App() {
+  const [cityName, setCityName] = useState([]);
+  const [localTemp, setlocalTemp] = useState([]);
+  const [localWeather, setLocalWeather] = useState([]);
+
+  
+  async function getWeather() {
+    const response = await fetch('https://api.openweathermap.org/data/2.5/forecast/daily?q=London&units=imperial&cnt=7&appid=08e194dbf106b4a880235ddea75704dd');
+    const data = await response.json()
+
+    console.log(data)
+       setlocalTemp(data.list[0].temp.day) 
+    setLocalWeather(data.list[0].weather[0].main)
+    setCityName(data.city.name)
+
+
+    }
+    
+    
+  
+  console.log(localTemp)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      
+      <Weather temperature={localTemp} weather={localWeather} city={cityName}/>
+      <Button variant="contained" onClick={getWeather}>
+  Get Weather
+</Button>
+     
     </div>
   );
 }
