3a0db7177eac31401a000fc3af7c1f43586085a8
Brinelson <ladofmusic@gmail.com>
2022-03-21 12:14:06 -0600

    unit type fixed

--- src/App.js
+++ src/App.js
@@ -41,13 +41,15 @@ function App() {
   const [localWeather, setLocalWeather] = useState([]);
   const [dayList, setDayList] = useState([]);
   const [chosenCity, setChosenCity] = useState('');
+  const [chosenUnit, setChosenUnit] = useState('imperial');
+  
 
 
   
   async function getWeather() {
 
 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${chosenCity}&units=imperial&cnt=7&appid=08e194dbf106b4a880235ddea75704dd`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${chosenCity}&units=${chosenUnit}&cnt=7&appid=08e194dbf106b4a880235ddea75704dd`);
     const data = await response.json()
 
     console.log(data.list)
@@ -71,7 +73,11 @@ function App() {
         type="text"
           value={chosenCity}
           onChange={(e) => setChosenCity(e.target.value)}
-      ></input>
+          ></input>
+          <select onChange={(e) => setChosenUnit(e.target.value)} name="units" id="units">
+  <option value="metric">Metric</option>
+  <option value="imperial" selected>Imperial</option>
+</select>
       
       </form>
       <Button variant="contained" onClick={getWeather}>
